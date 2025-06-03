06f3288005cf1cecec5d96bfe57d150e1235f85c
asai <yuanqian.mail@gmail.com>
2025-06-03 11:05:54 +0800

    create new project

---
+++ src/main/kotlin/com/nyanko/nyankokurumabackend/NyankokurumaBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.nyanko.nyankokurumabackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NyankokurumaBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<NyankokurumaBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/nyanko/nyankokurumabackend/NyankokurumaBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nyanko.nyankokurumabackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NyankokurumaBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
